73df463ce4417cfadb15a30dec34ed60dc26b672
TracyTN <tracytn0411@gmail.com>
2019-08-26 18:32:06 -0700

    Basic train scheduler logic  with firebase database.

--- README.md
+++ README.md
@@ -17,11 +17,11 @@ Make sure that your app suits this basic spec:
 - When adding trains, administrators should be able to submit the following:
   - Train Name
   - Destination
-  - First Trai  - me -- in military time
+  - First Train Time -- in military time
   - Frequency -- in minutes
-  - Code this app to calculate when the next train will arrive; this should be relative to the current time.
-  - Users from many different machines must be able to view same train times.
-  - Styling and theme are completely up to you. Get Creative!
+- Code this app to calculate when the next train will arrive; this should be relative to the current time.
+- Users from many different machines must be able to view same train times.
+- Styling and theme are completely up to you. Get Creative!
 
 ### :star2: Bonus :star2: (Extra Challenges)
 
---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+var displayTime;
+
+//Get current time with Moment.js
+function updateTime() {
+  displayTime = moment().format("MMMM Do YYYY, HH:mm:ss");
+  $("#currentTime").html(displayTime);
+}
+
+//run updateTime after html is loaded
+$(function () {
+  updateTime();
+  setInterval(updateTime, 1000); //time counter
+});
+
+// Firebase configuration
+var config = {
+  apiKey: "AIzaSyAXkYSDZX56imKl1taI93THOOHF2vzdmCY",
+  authDomain: "train-scheduler-7ce67.firebaseapp.com",
+  databaseURL: "https://train-scheduler-7ce67.firebaseio.com",
+  projectId: "train-scheduler-7ce67",
+  storageBucket: "",
+  messagingSenderId: "135693448620",
+  appId: "1:135693448620:web:917dd9d1412e2e0c"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#addSchedule").on("click", function(){
+  var trainName = $("#inputTrainName").val().trim();
+  var trainDestination = $("#inputDestination").val().trim();
+  var trainFirstTime = $("#inputFirstTrain").val();
+  var trainFrequency = $("#inputFrequency").val().trim();
+
+  console.log(trainFirstTime);
+  var newTrain = {
+    name : trainName,
+    destination: trainDestination,
+    first_train: trainFirstTime,
+    frequency: trainFrequency,
+  }
+
+  database.ref().push(newTrain);
+
+  $("form").trigger("reset"); //clear form field 
+
+  return false; //prevent page reloading
+});
+
+database.ref().on("child_added", function (snapshot) {
+  var fireData = snapshot.val();
+  var timeFirst = fireData.first_train; // time: display time, t: calc time
+  var timeFrequency = fireData.frequency;
+  
+  //Convert time of first train 
+  var tFirstConverter = moment(timeFirst, "HH:mm")
+ 
+  //Time difference (in minutes) between the first train and current time
+  var tDiff = moment().diff(moment(tFirstConverter), "minutes")
+
+  //Time has passed until next arrival
+  var tPassed = tDiff % timeFrequency;
+
+  //Next train arrival time
+  var tMinuteTillArrival = timeFrequency - tPassed;
+  var tArrival = moment().add(tMinuteTillArrival, "minutes");
+  var timeArrival = moment(tArrival).format("HH:mm");
+
+  //Update html table
+  var tableRow = $("<tr>").append([
+    $("<td>").text(fireData.name),
+    $("<td>").text(fireData.destination),
+    $("<td>").text(fireData.frequency),
+    $("<td>").text(timeArrival),
+    $("<td>").text(tMinuteTillArrival)
+  ]);
+
+  $("#newTrain").append(tableRow);
+});
+
+
